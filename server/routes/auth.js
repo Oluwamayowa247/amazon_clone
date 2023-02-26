7ad22b19cefa36038aaeffda2e9050f8ad87b082
Oluwamayowa247 <mayowaolutuyo@gmail.com>
2023-02-26 12:31:18 +0100

    sign-in in progress

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,6 +2,7 @@ const express = require("express");
 const User = require("../models/user");
 //bcrypt : alllows for encryption of password
 const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const authRouter = express.Router();
 
 // authRouter.get('/user', (req, res)=>{
@@ -17,10 +18,10 @@ authRouter.post("/api/signup", async (req, res) => {
     const existingUser = await User.findOne({ email });
 
     if (existingUser) {
-      return res.status(400).json({ msg: "user with same email exists!!!" });
+      return res.status(400).json({ msg: "User with same email exists!!!" });
     }
 
-   const hashedPassword = await bcryptjs.hash(password,8 );
+    const hashedPassword = await bcryptjs.hash(password, 8);
     //let keyword allows for scope variable declaration
     let user = new User({
       email,
@@ -32,13 +33,39 @@ authRouter.post("/api/signup", async (req, res) => {
     res.json(user);
   } catch (e) {
     //status code 500 : Internal Server error
-    res.status(500).json({error: e.message});
+    res.status(500).json({ error: e.message });
   }
   //post data in database
 
   //return data to user
 });
 
+//Sign In Route
+authRouter.post("/api/signin", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    //Check if user exist
+    if (!user) {
+      //Status code 400 : Bad request
+      return res
+        .status(400)
+        .json({ msg: "User with this email does not exist !!!" });
+    }
+   const matchedPassword = await bcryptjs.compare(password, user.password );
+
+   if(!matchedPassword){
+    return  res
+    .status(400)
+    .json({ msg: "Incorrect password !!!" });
+   }
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
+
 //expose file for public usage
+
 //to export mulitple things objects or maps can be created
 module.exports = authRouter;
